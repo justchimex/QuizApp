54848c7ee82b468a55615781a21d06c0677557ee
justchimex <117735991+justchimex@users.noreply.github.com>
2024-08-27 18:37:01 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -12,13 +12,13 @@ let questions = [
 },
 {
 	num: 2,
-	question: "What does CSS stand for?",
+	question: "Which technology is primarily responsible for the styling of web pages?",
 	answer: "B. Cascading Style Sheet",
 	options: [
-		"A. Code Style Sheet",
+		"A. JavaScript",
 		"B. Cascading Style Sheet",
-		"C. Computer Style Sheet",
-		"D. Codial Style Sheet"
+		"C. HTML",
+		"D. Python"
 		]
 },
 {
@@ -53,6 +53,500 @@ let questions = [
 		"C. Computer Processing Unit",
 		"D. Central Processing Unit"
 		]
+},
+{
+	num: 6,
+	question: "Which programming language is mainly used for adding interactivity to websites?",
+	answer: "D. JavaScript",
+	options: [
+		"A. HTML",
+		"B. CSS",
+		"C. Python",
+		"D. Javascript"
+		]
+},
+{
+	num: 7,
+	question: "What is the purpose of a front-end web development framework like React or Angular?",
+	answer: "B. To create a visually appealing user interface",
+	options: [
+		"A. To manage databases and server-side logic",
+		"B. To create a visually appealing user interface",
+		"C. To handle server-side routing",
+		"D. To interact with web servers"
+		]
+},
+{
+	num: 8,
+	question: "Which part of web development is responsible for handling data storage and retrieval?",
+	answer: "B. Back-end development",
+	options: [
+		"A. Front-end development",
+		"B. Back-end development",
+		"C. Full-stack development",
+		"D. Middleware development"
+		]
+},
+{
+	num: 9,
+	question: "What is the primary function of a web server in the context of web development?",
+	answer: "D. Handling HTTP requests and serving web pages",
+	options: [
+		"A. Rendering web pages on the client’s browser",
+		"B. Executing JavaScript code",
+		"C. Storing user data",
+		"D. Handling HTTP requests and serving web pages"
+		]
+},
+{
+	num: 10,
+	question: "Which of the following is not a back-end programming language commonly used in web development?",
+	answer: "D. HTML",
+	options: [
+		"A. PHP",
+		"B. Ruby",
+		"C. Java",
+		"D. HTML"
+		]
+},
+{
+	num: 11,
+	question: "Which type of web development allows for both front-end and back-end development using a single language",
+	answer: "A. Full-stack development",
+	options: [
+		"A. Full-stack development",
+		"B. Cross-platform development",
+		"C. Multi-language development",
+		"D. Hybrid development"
+		]
+},
+{
+	num: 12,
+	question: "What is the purpose of the script tag in HTML?",
+	answer: "C. To include external JavaScript code",
+	options: [
+		"A. To define the page’s structure",
+		"B. To include external CSS styles",
+		"C. o include external JavaScript code",
+		"D. To create hyperlinks"
+		]
+},
+{
+	num: 13,
+	question: "Which HTML tag is used to create a hyperlink?",
+	answer: "A. {a}",
+	options: [
+		"A. {a}",
+		"B. {link}",
+		"C. {h1}",
+		"D. {p}"
+		]
+},
+{
+	num: 14,
+	question: "Which CSS property is used to control the spacing between elements in a layout?",
+	answer: "B. margin",
+	options: [
+		"A. padding",
+		"B. margin",
+		"C. spacing",
+		"D. border"
+		]
+},
+{
+	num: 15,
+	question: "Which of the following is a popular front-end development framework maintained by Google?",
+	answer: "B. Angular",
+	options: [
+		"A. React",
+		"B. Angular",
+		"C. Vue.js",
+		"D. Django"
+		]
+},
+{
+	num: 16,
+	question: "Which of the following is a server-side scripting language commonly used for web development?",
+	answer: "C. Python",
+	options: [
+		"A. HTML",
+		"B. CSS",
+		"C. Python",
+		"D. Javascript"
+		]
+},
+{
+	num: 17,
+	question: "Which technology is primarily responsible for the styling of web pages?",
+	answer: "B. Cascading Style Sheet",
+	options: [
+		"A. JavaScript",
+		"B. Cascading Style Sheet",
+		"C. HTML",
+		"D. Python"
+		]
+},
+{
+	num: 18,
+	question: "What does SQL stand for?",
+	answer: "A. Structured Query Language",
+	options: [
+		"A. Structured Query Language",
+		"B. Strength Query Language",
+		"C. Stylesheet Query Language",
+		"D. Systematic Query Language"
+		]
+},
+{
+	num: 19,
+	question: "What does PHP stand for?",
+	answer: "A. Hypertext Preprocessor",
+	options: [
+		"A. Hypertext Preprocessor",
+		"B. Programming Processor",
+		"C. Hypertext Preprogramming",
+		"D. Homemade Processor"
+		]
+},
+{
+	num: 20,
+	question: "What does CPU stand for",
+	answer: "D. Central Processing Unit",
+	options: [
+		"A. Central Protective Unit",
+		"B. Control Processing Unit",
+		"C. Computer Processing Unit",
+		"D. Central Processing Unit"
+		]
+},
+{
+	num: 21,
+	question: "What does the acronym HTML stand for?",
+	answer: "D. Hyper Text Markup Language",
+	options: [
+		"A. Hyper Type Multi Language",
+		"B. Hyper Type Markup Language",
+		"C. Hyper Text Multiple Language",
+		"D. Hyper Text Markup Language"
+		]
+},
+{
+	num: 22,
+	question: "Which technology is primarily responsible for the styling of web pages?",
+	answer: "B. Cascading Style Sheet",
+	options: [
+		"A. JavaScript",
+		"B. Cascading Style Sheet",
+		"C. HTML",
+		"D. Python"
+		]
+},
+{
+	num: 23,
+	question: "What does SQL stand for?",
+	answer: "A. Structured Query Language",
+	options: [
+		"A. Structured Query Language",
+		"B. Strength Query Language",
+		"C. Stylesheet Query Language",
+		"D. Systematic Query Language"
+		]
+},
+{
+	num: 24,
+	question: "What does PHP stand for?",
+	answer: "A. Hypertext Preprocessor",
+	options: [
+		"A. Hypertext Preprocessor",
+		"B. Programming Processor",
+		"C. Hypertext Preprogramming",
+		"D. Homemade Processor"
+		]
+},
+{
+	num: 25,
+	question: "What does CPU stand for",
+	answer: "D. Central Processing Unit",
+	options: [
+		"A. Central Protective Unit",
+		"B. Control Processing Unit",
+		"C. Computer Processing Unit",
+		"D. Central Processing Unit"
+		]
+},
+{
+	num: 26,
+	question: "What does the acronym HTML stand for?",
+	answer: "D. Hyper Text Markup Language",
+	options: [
+		"A. Hyper Type Multi Language",
+		"B. Hyper Type Markup Language",
+		"C. Hyper Text Multiple Language",
+		"D. Hyper Text Markup Language"
+		]
+},
+{
+	num: 27,
+	question: "Which technology is primarily responsible for the styling of web pages?",
+	answer: "B. Cascading Style Sheet",
+	options: [
+		"A. JavaScript",
+		"B. Cascading Style Sheet",
+		"C. HTML",
+		"D. Python"
+		]
+},
+{
+	num: 28,
+	question: "What does SQL stand for?",
+	answer: "A. Structured Query Language",
+	options: [
+		"A. Structured Query Language",
+		"B. Strength Query Language",
+		"C. Stylesheet Query Language",
+		"D. Systematic Query Language"
+		]
+},
+{
+	num: 29,
+	question: "What does PHP stand for?",
+	answer: "A. Hypertext Preprocessor",
+	options: [
+		"A. Hypertext Preprocessor",
+		"B. Programming Processor",
+		"C. Hypertext Preprogramming",
+		"D. Homemade Processor"
+		]
+},
+{
+	num: 30,
+	question: "What does CPU stand for",
+	answer: "D. Central Processing Unit",
+	options: [
+		"A. Central Protective Unit",
+		"B. Control Processing Unit",
+		"C. Computer Processing Unit",
+		"D. Central Processing Unit"
+		]
+},
+{
+	num: 31,
+	question: "What does the acronym HTML stand for?",
+	answer: "D. Hyper Text Markup Language",
+	options: [
+		"A. Hyper Type Multi Language",
+		"B. Hyper Type Markup Language",
+		"C. Hyper Text Multiple Language",
+		"D. Hyper Text Markup Language"
+		]
+},
+{
+	num: 32,
+	question: "Which technology is primarily responsible for the styling of web pages?",
+	answer: "B. Cascading Style Sheet",
+	options: [
+		"A. JavaScript",
+		"B. Cascading Style Sheet",
+		"C. HTML",
+		"D. Python"
+		]
+},
+{
+	num: 33,
+	question: "What does SQL stand for?",
+	answer: "A. Structured Query Language",
+	options: [
+		"A. Structured Query Language",
+		"B. Strength Query Language",
+		"C. Stylesheet Query Language",
+		"D. Systematic Query Language"
+		]
+},
+{
+	num: 34,
+	question: "What does PHP stand for?",
+	answer: "A. Hypertext Preprocessor",
+	options: [
+		"A. Hypertext Preprocessor",
+		"B. Programming Processor",
+		"C. Hypertext Preprogramming",
+		"D. Homemade Processor"
+		]
+},
+{
+	num: 35,
+	question: "What does CPU stand for",
+	answer: "D. Central Processing Unit",
+	options: [
+		"A. Central Protective Unit",
+		"B. Control Processing Unit",
+		"C. Computer Processing Unit",
+		"D. Central Processing Unit"
+		]
+},
+{
+	num: 36,
+	question: "What does the acronym HTML stand for?",
+	answer: "D. Hyper Text Markup Language",
+	options: [
+		"A. Hyper Type Multi Language",
+		"B. Hyper Type Markup Language",
+		"C. Hyper Text Multiple Language",
+		"D. Hyper Text Markup Language"
+		]
+},
+{
+	num: 37,
+	question: "Which technology is primarily responsible for the styling of web pages?",
+	answer: "B. Cascading Style Sheet",
+	options: [
+		"A. JavaScript",
+		"B. Cascading Style Sheet",
+		"C. HTML",
+		"D. Python"
+		]
+},
+{
+	num: 38,
+	question: "What does SQL stand for?",
+	answer: "A. Structured Query Language",
+	options: [
+		"A. Structured Query Language",
+		"B. Strength Query Language",
+		"C. Stylesheet Query Language",
+		"D. Systematic Query Language"
+		]
+},
+{
+	num: 39,
+	question: "What does PHP stand for?",
+	answer: "A. Hypertext Preprocessor",
+	options: [
+		"A. Hypertext Preprocessor",
+		"B. Programming Processor",
+		"C. Hypertext Preprogramming",
+		"D. Homemade Processor"
+		]
+},
+{
+	num: 40,
+	question: "What does CPU stand for",
+	answer: "D. Central Processing Unit",
+	options: [
+		"A. Central Protective Unit",
+		"B. Control Processing Unit",
+		"C. Computer Processing Unit",
+		"D. Central Processing Unit"
+		]
+},
+{
+	num: 41,
+	question: "What does the acronym HTML stand for?",
+	answer: "D. Hyper Text Markup Language",
+	options: [
+		"A. Hyper Type Multi Language",
+		"B. Hyper Type Markup Language",
+		"C. Hyper Text Multiple Language",
+		"D. Hyper Text Markup Language"
+		]
+},
+{
+	num: 42,
+	question: "Which technology is primarily responsible for the styling of web pages?",
+	answer: "B. Cascading Style Sheet",
+	options: [
+		"A. JavaScript",
+		"B. Cascading Style Sheet",
+		"C. HTML",
+		"D. Python"
+		]
+},
+{
+	num: 43,
+	question: "What does SQL stand for?",
+	answer: "A. Structured Query Language",
+	options: [
+		"A. Structured Query Language",
+		"B. Strength Query Language",
+		"C. Stylesheet Query Language",
+		"D. Systematic Query Language"
+		]
+},
+{
+	num: 44,
+	question: "What does PHP stand for?",
+	answer: "A. Hypertext Preprocessor",
+	options: [
+		"A. Hypertext Preprocessor",
+		"B. Programming Processor",
+		"C. Hypertext Preprogramming",
+		"D. Homemade Processor"
+		]
+},
+{
+	num: 45,
+	question: "What does CPU stand for",
+	answer: "D. Central Processing Unit",
+	options: [
+		"A. Central Protective Unit",
+		"B. Control Processing Unit",
+		"C. Computer Processing Unit",
+		"D. Central Processing Unit"
+		]
+},
+{
+	num: 46,
+	question: "What does the acronym HTML stand for?",
+	answer: "D. Hyper Text Markup Language",
+	options: [
+		"A. Hyper Type Multi Language",
+		"B. Hyper Type Markup Language",
+		"C. Hyper Text Multiple Language",
+		"D. Hyper Text Markup Language"
+		]
+},
+{
+	num: 47,
+	question: "Which technology is primarily responsible for the styling of web pages?",
+	answer: "B. Cascading Style Sheet",
+	options: [
+		"A. JavaScript",
+		"B. Cascading Style Sheet",
+		"C. HTML",
+		"D. Python"
+		]
+},
+{
+	num: 48,
+	question: "What does SQL stand for?",
+	answer: "A. Structured Query Language",
+	options: [
+		"A. Structured Query Language",
+		"B. Strength Query Language",
+		"C. Stylesheet Query Language",
+		"D. Systematic Query Language"
+		]
+},
+{
+	num: 49,
+	question: "What does PHP stand for?",
+	answer: "A. Hypertext Preprocessor",
+	options: [
+		"A. Hypertext Preprocessor",
+		"B. Programming Processor",
+		"C. Hypertext Preprogramming",
+		"D. Homemade Processor"
+		]
+},
+{
+	num: 50,
+	question: "What does CPU stand for",
+	answer: "D. Central Processing Unit",
+	options: [
+		"A. Central Protective Unit",
+		"B. Control Processing Unit",
+		"C. Computer Processing Unit",
+		"D. Central Processing Unit"
+		]
 }
-
 	];
