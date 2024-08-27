9e96e071356ec46f15233d811d00586876fa17bb
justchimex <117735991+justchimex@users.noreply.github.com>
2024-08-27 01:06:06 +0100

    Update questions.js

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+{
+	num: 1,
+	question: "What does the acronym HTML stand for?",
+	answer: "D. Hyper Text Markup Language",
+	options: [
+		"A. Hyper Type Multi Language",
+		"B. Hyper Type Markup Language",
+		"C. Hyper Text Multiple Language",
+		"D. Hyper Text Markup Language"
+		]
+},
+{
+	num: 2,
+	question: "What does CSS stand for?",
+	answer: "B. Cascading Style Sheet",
+	options: [
+		"A. Code Style Sheet",
+		"B. Cascading Style Sheet",
+		"C. Computer Style Sheet",
+		"D. Codial Style Sheet"
+		]
+},
+{
+	num: 3,
+	question: "What does SQL stand for?",
+	answer: "A. Structured Query Language",
+	options: [
+		"A. Structured Query Language",
+		"B. Strength Query Language",
+		"C. Stylesheet Query Language",
+		"D. Systematic Query Language"
+		]
+},
+{
+	num: 4,
+	question: "What does PHP stand for?",
+	answer: "A. Hypertext Preprocessor",
+	options: [
+		"A. Hypertext Preprocessor",
+		"B. Programming Processor",
+		"C. Hypertext Preprogramming",
+		"D. Homemade Processor"
+		]
+},
+{
+	num: 5,
+	question: "What does CPU stand for",
+	answer: "D. Central Processing Unit",
+	options: [
+		"A. Central Protective Unit",
+		"B. Control Processing Unit",
+		"C. Computer Processing Unit",
+		"D. Central Processing Unit"
+		]
+}
+
+	];
